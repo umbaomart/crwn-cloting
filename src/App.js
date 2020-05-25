3ac2a667322fd7091a44c51f8c780e8c49dfa186
umbaomart <umbaomart@gmail.com>
2020-05-25 17:48:50 +0800

    adding header fcomponent and lin to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
+// Components
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.componet'
 
 // eslint-disable-next-line
 const HatsPage = () => (
@@ -16,6 +18,7 @@ const HatsPage = () => (
 function App () {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
